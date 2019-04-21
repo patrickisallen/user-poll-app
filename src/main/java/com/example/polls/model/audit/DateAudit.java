cf42879c64b12c7cb1a3331e8f31bb0bca66299d
Patrick <patrick.allen.ruiz@gmail.com>
2019-04-20 19:49:03 -0700

    init push

---
+++ src/main/java/com/example/polls/model/audit/DateAudit.java
@@ -0,0 +1,22 @@
+package com.example.polls.model.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+
+public abstract class DateAudit implements Serializable {
+
+}
